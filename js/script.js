5d23d1c102b0dcbfecf1c51150dd9f556311f471
JonathanAndrew <jonandrewstevenson@yahoo.com>
2016-06-16 11:41:23 -0700

    updated swipping

--- js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-   $("#carousel-id").swiperight(function() {  
-      $("#myCarousel").carousel('prev');  
-    });  
-   $("#carousel-id").swipeleft(function() {  
-      $("#myCarousel").carousel('next');  
-   });  
-});  
--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+	$("#carousel-id").swiperight(function() {  
+      $("#myCarousel").carousel('prev');  
+    });  
+   $("#carousel-id").swipeleft(function() {  
+      $("#myCarousel").carousel('next');  
+   	});  
 	$('#carousel-id').carousel({
   		interval: 3000
 	});
