650ba37498dd94b040d898acf9e76ac927c44a9a
JonathanAndrew <jonandrewstevenson@yahoo.com>
2016-06-16 17:57:55 -0700

    udpate

--- static/js/script.js
+++ static/js/script.js
@@ -6,13 +6,13 @@ $(document).ready(function(){
    	$("#carousel-id").swipeleft(function() {  
       	$(this).carousel('next');  
 	});  
-	$(".left").on('click',function(){
-		$(this).carousel('prev');
-	});
-	$(".right").on('click',function(){
-		$(this).carousel('next');
-	})
-	$('#carousel-id').carousel({
+	// $("#carousel-id").on('click',function(){
+	// 	$(this).carousel('prev');
+	// });
+	// $(".carousel-control").on('click',function(){
+	// 	$(this).carousel('next');
+	// })
+	$('.carousel-control').carousel({
   		interval: 3000
 	});
 	var portraits = ['WEDDING','ENGAGEMENT','GRADUATION','MATERNITY','ANIMALS','FAMILY'];
