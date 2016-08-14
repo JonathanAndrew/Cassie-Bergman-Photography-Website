4d1bec5cbc56fd116bd789036542542461f23d12
JonathanAndrew <jonandrewstevenson@yahoo.com>
2016-08-14 12:44:22 -0700

    updates

--- static/js/script.js
+++ static/js/script.js
@@ -17,12 +17,12 @@ $(document).ready(function(){
 	};
 
 	var engagementImages = [
-	{title: 'engagement-photo1', src: 'https://c3.staticflickr.com/8/7441/27111226490_d51135e18b_b.jpg', top:'40%'},
-	{title: 'engagement-photo2', src: 'https://c3.staticflickr.com/8/7741/27111225050_da7de3574a_b.jpg', top:'40%'},
-	{title: 'engagement-photo3', src: 'https://c2.staticflickr.com/8/7316/26780178713_6f9b87887e_b.jpg', top:'40%'},
-	{title: 'engagement-photo4', src: 'https://c2.staticflickr.com/8/7616/26780180673_584b6ea3ef_b.jpg', top:'40%'},
-	{title: 'engagement-photo5', src: 'https://c7.staticflickr.com/8/7616/27099120614_68b2e506f3_b.jpg', top:'40%'},
-	{title: 'engagement-photo6', src: 'https://c3.staticflickr.com/8/7073/27677491106_ce464e6f74_b.jpg', top:'40%'},
+	{title: 'engagement-photo1', src: 'https://c3.staticflickr.com/8/7441/27111226490_d51135e18b_b.jpg', top:'30%'},
+	{title: 'engagement-photo2', src: 'https://c3.staticflickr.com/8/7741/27111225050_da7de3574a_b.jpg', top:'30%'},
+	{title: 'engagement-photo3', src: 'https://c2.staticflickr.com/8/7316/26780178713_6f9b87887e_b.jpg', top:'30%'},
+	{title: 'engagement-photo4', src: 'https://c2.staticflickr.com/8/7616/26780180673_584b6ea3ef_b.jpg', top:'30%'},
+	{title: 'engagement-photo5', src: 'https://c7.staticflickr.com/8/7616/27099120614_68b2e506f3_b.jpg', top:'30%'},
+	{title: 'engagement-photo6', src: 'https://c3.staticflickr.com/8/7073/27677491106_ce464e6f74_b.jpg', top:'30%'},
 	{title: 'engagement-photo7', src: 'https://c3.staticflickr.com/8/7207/27111226010_d42c7f7f39_b.jpg', top:'0'},
 	{title: 'engagement-photo8', src: 'https://c6.staticflickr.com/8/7294/26780180293_fcf4bb17be_b.jpg', top:'0'},
 	{title: 'engagement-photo9', src: 'https://c1.staticflickr.com/8/7419/27677579776_a86f183b73_b.jpg', top:'0'}
